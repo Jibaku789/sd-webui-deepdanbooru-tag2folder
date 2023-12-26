018d0d81eeb49df4c9a952abc66761f29c12d5da
Jibaku789 <151478027+Jibaku789@users.noreply.github.com>
2023-12-26 14:24:24 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# stable-diffusion-webui-deepdanbooru-tag2folder
+
+Using this script you can move images using deepdanbooru classification
+
+Version 1.0
+* Add initial commit and create repository
+
+## Rules Explanation
+
+The <Rules> section is a text area that will be filled using the rules to migrate the folder.
+This extension reads the images in the <Source Folder> and then match the image with the rules.
+
+The rules should be written in the following format:
+{
+	"subfolder1": {
+		"AND": ["tag1", "tag2"],
+	},
+	"subfolder2": {
+		"OR": ["tag1", "tag2"],
+	}
+}
+
+Depending of the condition "AND" or "OR" will match the image to be moved in the subfolder.
+Using "AND" will match all the tags in the list and using "OR" will match only one tag.
+In case that an image does not match any of the rules, will be moved to a folder called "unclassified".
+
+Additionally there are two more options in the <Automatic Type>, in case of the images does not belong to any rule. This options can create subfolders depending of the type
+- None: will move the images to unclassified
+- Anime: will fetch the anime serie that belong the image and create a new folder with that anime name
+- Character: will fetch the character name that belong the image and create a new folder with that character name
+
+You can review teh example input_folder and input_rules.json in the examples folder
+
+![Interface](https://github.com/Jibaku789/sd-webui-deepdanbooru-tag2folder/blob/main/examples/interface.png)
---
+++ scripts/deepdanbooru_tag2folder.py
@@ -0,0 +1,248 @@
+"""
+    @author: Jibaku789
+    @version: 1.0
+    @date: December 2023
+"""
+
+import gradio as gr
+import os
+import time
+import torch
+import numpy as np
+import json
+import re
+from PIL import Image
+
+from modules import script_callbacks
+from modules import devices, images
+from modules.deepbooru import DeepDanbooru
+
+class DeepDanbooruWrapper:
+
+    def __init__(
+        self,
+    ):
+
+        self.dd_classifier = DeepDanbooru()
+        self.cache = {}
+        self.enable_cache = False
+
+    def start(self):
+        print("Starting DeepDanboru")
+        self.dd_classifier.start()
+
+    def stop(self):
+        print("Stopping DeepDanboru")
+        self.dd_classifier.stop()
+
+    def evaluate_model(self, pil_image, image_id="", minimal_threshold=0):
+
+        if self.enable_cache:
+            if image_id and image_id in self.cache:
+                return self.cache[image_id]
+
+        # Input image should be 512x512 before reach this point
+        pic = images.resize_image(0, pil_image.convert("RGB"), 512, 512)
+
+        #pic = pil_image.convert("RGB")
+        a = np.expand_dims(np.array(pic, dtype=np.float32), 0) / 255
+
+        with torch.no_grad(), devices.autocast():
+            x = torch.from_numpy(a).to(devices.device)
+            y = self.dd_classifier.model(x)[0].detach().cpu().numpy()
+
+        probability_dict = {}
+        for tag, probability in zip(self.dd_classifier.model.tags, y):
+
+            if probability < minimal_threshold:
+                continue
+
+            if tag.startswith("rating:"):
+                continue
+
+            probability_dict[tag] = probability
+
+        if self.enable_cache:
+            self.cache[image_id] = probability_dict
+
+        return probability_dict
+
+class DeepDanbooruTag2FolderScript():
+
+    def __init__(self):
+
+        self.source_folder = None
+        self.target_folder = None
+        self.threshold = None
+        self.rules = None
+        self.auto_type = None
+        self.process_btn = None
+
+    def on_ui_tabs(self):
+
+        with gr.Blocks(analytics_enabled=False) as ui_component:
+
+            sample = {"<subfolder_name>": {"OR": ["<tag1>", "<tag2>"]}}
+
+            with gr.Row():
+
+                with gr.Column(scale=1, elem_classes="source-image-col"):
+                    self.source_folder = gr.Textbox(value="", label="Source Folder", elem_id="source_folder")
+                    self.target_folder = gr.Textbox(value="", label="Target Folder", elem_id="target_folder")
+                    self.auto_type = gr.Dropdown(["None", "Character", "Anime"], value="None", label="Automatic Type", elem_id="auto_type")
+                    self.threshold = gr.Number(value=0.5, label="Threshold", elem_id="threshold", minimum=0, maximum=1)
+
+                with gr.Column(scale=1, elem_classes="other elements"):
+                    self.rules = gr.Textbox(value=json.dumps(sample, indent=True), lines=10, label="Rules", elem_id="rules_json")
+
+            with gr.Row():
+                self.process_btn = gr.Button(value="Process", elem_id="process_btn")
+
+            self.process_btn.click(
+                self.ui_click,
+                inputs=[
+                    self.source_folder,
+                    self.target_folder,
+                    self.threshold,
+                    self.rules,
+                    self.auto_type
+                ]
+            )
+
+            return [(ui_component, "DeepDanboru Tag2Folder", "deepdanboru_tag2folder_tab")]
+
+
+    def ui_click(self, source_folder, target_folder, threshold, rules, auto_type):
+
+        json_rules = json.loads(rules)
+        source_files = os.listdir(source_folder)
+        metrics = {}
+        start_time = time.time()
+
+        dd_wrapper = DeepDanbooruWrapper()
+        print("Loading DeepDanboru")
+        dd_wrapper.start()
+
+        print(f"Start Processing of {len(source_files)} files")
+        count = -1
+        for _file in source_files:
+            count += 1
+
+            if count % 10 == 0:
+                print(f"Progress {count}/{len(source_files)}. Elapsed: {time.time() - start_time} segs")
+
+            source_filename = os.path.join(source_folder, _file)
+
+            try:
+                with Image.open(source_filename) as pil_image:
+
+                    # Evaluate model
+                    model_tags = dd_wrapper.evaluate_model(
+                        pil_image,
+                        source_filename,
+                        threshold
+                    )
+
+                    # Move images to new folders
+                    found = False
+                    for subfolder, conditions in json_rules.items():
+
+                        new_folder = os.path.join(target_folder, subfolder)
+                        if not os.path.exists(new_folder):
+                            os.makedirs(new_folder)
+
+                        if "AND" in conditions and conditions["AND"]:
+
+                            to_add = True
+                            for tag in conditions["AND"]:
+
+                                in_mtag = False
+                                for mtag in model_tags:
+
+                                    if "partial:" in tag:
+                                        rtag = tag.replace("partial:", "").strip()
+                                        if rtag in mtag:
+                                            in_mtag = True
+                                            break
+                                    else:
+                                        if tag == mtag:
+                                            in_mtag = True
+                                            break
+
+                                to_add = to_add and in_mtag
+
+                            if to_add:
+                                found = True
+                                pil_image.save(os.path.join(new_folder, _file))
+
+                        if "OR" in conditions and conditions["OR"]:
+
+                            to_add = False
+                            for tag in conditions["OR"]:
+
+                                in_mtag = False
+                                for mtag in model_tags:
+
+                                    if "partial:" in tag:
+                                        rtag = tag.replace("partial:", "").strip()
+                                        if rtag in mtag:
+                                            in_mtag = True
+                                            break
+                                    else:
+                                        if tag == mtag:
+                                            in_mtag = True
+                                            break
+
+                                to_add = to_add or in_mtag
+
+                            if to_add:
+                                found = True
+                                pil_image.save(os.path.join(new_folder, _file))
+
+
+                    if not found:
+
+                        new_folder = os.path.join(target_folder, "unclassified")
+
+                        if auto_type != "None":
+                            for tag in model_tags:
+
+                                if auto_type == "Anime":
+                                    match = re.search("\((.*)\)", tag)
+                                    if match:
+                                        new_folder = os.path.join(target_folder, match.group(1))
+                                        break
+
+                                elif auto_type == "Character":
+                                    if "(" in tag:
+                                        new_folder = os.path.join(target_folder, tag)
+                                        break
+
+                        if not os.path.exists(new_folder):
+                            os.makedirs(new_folder)
+
+                        pil_image.save(os.path.join(new_folder, _file))
+
+                    # Update metrics
+                    for tag in model_tags:
+                        if tag in metrics:
+                            metrics[tag] += 1
+                        else:
+                            metrics[tag] = 1
+
+            except Exception as e:
+                print(e)
+
+        # Save metrics
+        metrics = dict(sorted(metrics.items(), key=lambda x: -x[1]))
+        with open(os.path.join(target_folder, "metrics.json"), "w") as _f:
+            _f.write(json.dumps(metrics, indent=True))
+
+        dd_wrapper.stop()
+        print(f"Finish in {time.time() - start_time} segs")
+
+
+script = DeepDanbooruTag2FolderScript()
+script_callbacks.on_ui_tabs(script.on_ui_tabs)
+
+# end of file
